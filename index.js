0d750219d056ec7acc0ae926f813c0e36b8c7919
Jasurbek <jasurdev1604@gmail.com>
2024-04-03 14:11:44 +0500

    Contructor Function

---
+++ index.js
@@ -0,0 +1,15 @@
+function User(name, age, country) {
+  this.name = name;
+  this.age = age;
+  if (!country || !country.trim()) {
+    this.country = "NONE";
+  } else {
+    this.country = country;
+  }
+}
+
+const user1 = new User("jasurbek", 19);
+const user2 = new User("none", 50, "uz");
+
+console.log(user1);
+console.log(user2);
