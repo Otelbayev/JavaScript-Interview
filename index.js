959f67cc2d38310fb3f602c79ca68b0602727760
Jasurbek <jasurdev1604@gmail.com>
2024-04-10 22:07:37 +0500

    Class, getter/setter, Inheritance

---
+++ index.js
@@ -0,0 +1,103 @@
+//!Constructor function
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+
+  this.getFullName = () => {
+    return this.name + " " + this.age;
+  };
+}
+
+const func1 = new User("John", 20);
+const func2 = new User("John", 20);
+
+// console.log(func1.getFullName());
+// console.log(func2);
+
+//!Class
+class User1 {
+  name = "";
+  age = "";
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getFullName() {
+    return this.name + " " + this.age;
+  }
+}
+
+const obj1 = new User1("John", 20);
+const obj2 = new User1("John", 20);
+
+// console.log(obj1.getFullName());
+// console.log(obj2);
+
+//!getter/setter
+
+class User2 {
+  _money = 0;
+  constructor(money) {
+    this._money = money;
+  }
+
+  get money() {
+    return this._money + 5;
+  }
+
+  set money(value) {
+    if (value > 100) {
+      this._money = value;
+    } else {
+      return this._money;
+    }
+  }
+}
+
+const user = new User2(500);
+
+// user.money = 100;
+
+// console.log(user.money);
+
+//!Inheritance
+
+class Person {
+  name = "";
+  lname = "";
+  _money = 0;
+
+  constructor(name, lname, money) {
+    this.name = name;
+    this.lname = lname;
+    this._money = money;
+  }
+
+  getFullName() {
+    return `${this.name} ${this.lname}`;
+  }
+
+  get money() {
+    return this._money;
+  }
+
+  spend(money) {
+    this._money -= money;
+  }
+}
+
+class Employee extends Person {
+  salary = 0;
+  constructor(name, lname, money, salary) {
+    super(name, lname, money);
+    this.salary = salary || 0;
+    this.earn();
+  }
+
+  earn() {
+    this._money += this.salary;
+  }
+}
+
+const emp = new Employee("Jasurbek", "Otelbayev", 50, 400);
