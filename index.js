6a269546cc682fe468211a1ce11f69d59c95742f
Jasurbek <jasurdev1604@gmail.com>
2024-07-15 21:44:14 +0500

    compose pipe referentisl

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ function sum(a) {
   };
 }
 
-const summa = sum(10)(20)(30);
-console.log(summa());
+// const summa = sum(10)(20)(30);
+// console.log(summa());
 
 function recursion(a, b) {
   if (b) {
@@ -37,4 +37,59 @@ function recursion(a, b) {
   return a;
 }
 
-console.log(recursion(10, 3));
+// console.log(recursion(10, 3));
+
+//!Compose function
+
+const compose = (a, b) => (str) => a(b(str));
+
+const countChars = (str) => {
+  const obj = {};
+
+  for (let char of str) {
+    obj[char] = obj[char] + 1 || 1;
+  }
+
+  return obj;
+};
+
+const findMaxChar = (obj) => {
+  let max = 0;
+  const result = { count: 0, char: "" };
+
+  for (let key in obj) {
+    if (obj[key] > max) {
+      max = obj[key];
+      result.count = obj[key];
+      result.char = key;
+    }
+  }
+  ``;
+  return result;
+};
+
+const getMaxChar = compose(findMaxChar, countChars);
+
+console.log(getMaxChar("Jasurbek Otelbayev"));
+
+//!Pipe function
+
+const pipe =
+  (...fns) =>
+  (x) =>
+    fns.reduce((data, fn) => fn(data), x);
+
+const addMeat = (osh) => osh + "Meat";
+const addEgg = (osh) => osh + "Egg";
+
+const osh = pipe(addMeat, addEgg);
+
+console.log(osh("OSH"));
+
+//! Referential transparency
+
+const summa = (a, b) => a + b;
+const showSum = (a) => "Result is " + a;
+
+console.log(showSum(summa(1, 2)));
+console.log(showSum(3));
