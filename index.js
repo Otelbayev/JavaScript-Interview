f94a2a8033c4fd45dfc76b76a204d0fa9fc343a2
Jasurbek <jasurdev1604@gmail.com>
2024-07-17 10:49:02 +0500

    EC phases

---
+++ index.js
@@ -0,0 +1,26 @@
+//!Execution context
+
+//Hosting in Caration phase
+
+// Global execution context
+let dfokdn = 10;
+var dfvof = 20;
+
+console.log("Jasubrek");
+
+console.log("none");
+
+function sayName() {
+  //Function execution context
+  let dklnv = "hseijk";
+  console.log("Hi");
+
+  function sayName2() {// function Executoin context
+
+    let dkjfvnv = "sdlkmslkfdr"
+
+    console.log("xkvjnsjkdnrw")
+
+  }
+
+}
